4b77fb47bef1a3d18b2333b1eac8b318f1927d26
VirmoP <116274275+VirmoP@users.noreply.github.com>
2023-03-10 22:13:09 +0200

    Tsipake ehitust

---
+++ cardjitsu/src/Kaart.java
@@ -0,0 +1,73 @@
+import java.util.Objects;
+
+public class Kaart {
+    private int tugevus;
+    private String element;
+
+    public Kaart(int tugevus, String element) {
+        this.tugevus = tugevus;
+        this.element = element;
+    }
+
+    public void KaartValja(){
+        System.out.println("┌───────────┐");
+        System.out.println("│"+" ".repeat(2)+tugevus+" ".repeat(8)+"│");
+        System.out.println("│"+" "+elementvalja()[0] +" "+"│");
+        System.out.println("│"+" "+elementvalja()[1] +" "+"│");
+        System.out.println("│"+" "+elementvalja()[2] +" "+"│");
+        System.out.println("│"+" "+elementvalja()[3] +" "+"│");
+        System.out.println("│"+" "+elementvalja()[4] +" "+"│");
+        System.out.println("└───────────┘");
+
+    }
+
+    private String[] elementvalja() {
+        if (Objects.equals(this.element, "tuli")) {
+            String[] uus = {
+                    "   |     ",
+                    "| ||  |  ",
+                    "|  ||  | ",
+                    "|||||||| ",
+                    " ||||||  ",
+            };
+            return uus;
+        }
+        if (Objects.equals(this.element, "vesi")){
+            String[] uus = {
+                    "   /\\    ",
+                    "  /  \\   ",
+                    " /    \\  ",
+                    "|      | ",
+                    " \\____/  ",
+            };
+            return uus;
+        }
+        if (Objects.equals(this.element, "ice")){
+            String[] uus = {
+                    " *  *  * ",
+                    "  \\ | /  ",
+                    "*===*===*",
+                    "  / | \\  ",
+                    " *  *  * ",
+            };
+            return uus;
+        }
+        return new String[]{"a", "b"};
+    }
+
+    public int getTugevus() {
+        return tugevus;
+    }
+
+    public void setTugevus(int tugevus) {
+        this.tugevus = tugevus;
+    }
+
+    public String getElement() {
+        return element;
+    }
+
+    public void setType(String element) {
+        this.element = element;
+    }
+}
--- cardjitsu/src/Main.java
+++ cardjitsu/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Kaart kaart1 = new Kaart(4, "ice");
+        Kaart kaart2 = new Kaart(4, "tuli");
+        Kaart kaart3 = new Kaart(4, "vesi");
+        kaart1.KaartValja();
+        kaart2.KaartValja();
+        kaart3.KaartValja();
     }
 }
