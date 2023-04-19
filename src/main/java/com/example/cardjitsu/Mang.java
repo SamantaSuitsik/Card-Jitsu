bec6ffd637b28c600b49a990c406f0663ead723d
VirmoP <116274275+VirmoP@users.noreply.github.com>
2023-04-19 17:23:58 +0300

    mang t;;tab n[[d tehniliselt

--- src/main/java/com/example/cardjitsu/Mang.java
+++ src/main/java/com/example/cardjitsu/Mang.java
@@ -21,6 +21,10 @@ public class Mang extends Application {
     private boolean mangkaib;
 
     private Canvas canvas;
+    private String mangijakestev = "";
+    private String vastasekestev = "";
+     private HashMap<String, Integer> mangijadict = new HashMap<>();
+    private HashMap<String, Integer> vastanedict = new HashMap<>();
 
     public Mang(){
         this.canvas = new Canvas(800, 600);
@@ -38,7 +42,18 @@ public class Mang extends Application {
         this.mangija.suvalisedKaardidKaes();
         this.vastane.suvalisedKaardidKaes();
         joonistaristkulikud();
+        joonistaMangijaKaardid();
+        setdictalgne();
 
+    }
+
+    public void setdictalgne(){
+        this.mangijadict.put("tuli", 0);
+        this.mangijadict.put("vesi", 0);
+        this.mangijadict.put("lumi", 0);
+        this.vastanedict.put("tuli", 0);
+        this.vastanedict.put("vesi", 0);
+        this.vastanedict.put("lumi", 0);
     }
 
     private void joonistaristkulikud() {
@@ -66,8 +81,9 @@ public class Mang extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         double yalgus = canvas.getHeight() * 3 / 4;
         double mangitud = ((8 * x + canvas.getWidth() / 10 * 4) / canvas.getWidth()) - 2;
-        if (y > yalgus && y < yalgus + canvas.getHeight() / 6) {
+        if (y > yalgus && y < yalgus + canvas.getHeight() / 6 && this.mangkaib) {
             if (mangitud - (int) mangitud <= 0.8) {
+                //TODO peab kontrollima et seda kaarti saaks k'ia
                 reageeri((int) mangitud);
                 gc.setFill(Color.INDIGO);
                 gc.fillRect(x, y, 10, 10);
@@ -77,9 +93,168 @@ public class Mang extends Application {
 
     public void reageeri(int indeks){
         Kaart mangitud = this.mangija.mangiKaart(indeks);
-        joonistaKaart(mangitud, 400, 200);
-        //TODO uus func migni teebackend vmidagi
 
+        //Prindib kõik mängja kaardid välja
+        joonistaMangijaKaardid();
+
+        //Prindib kõik võidetud kaartide kogused välja
+        joonistaVoiduKogused();
+
+        //Väljastab hetkel kehtivad efektid
+        joonistaEriEfektid();
+
+        //Prindib mängija käidud kaardi välja
+        joonistaKaart(mangitud, 400, 200);
+
+        //Kui eriline võime oli kaardi tugevust muuta teeb selle siin ära
+        if (this.mangijakestev.equals("+2"))
+            mangitud.setTugevus(mangitud.getTugevus()+2);
+        if (this.mangijakestev.equals("-2"))
+            mangitud.setTugevus(mangitud.getTugevus()-2);
+
+        joonistaKaart(mangitud, 400, 200);
+
+        //Prindib vastase käidud kaardi välja
+        Kaart vastanekaart = this.vastane.mangiKaart(this.mangijakestev);
+
+        joonistaKaart(vastanekaart, 200, 200);
+
+        //Kui eriline võime oli kaardi tugevust muuta teeb selle siin ära
+        if (vastasekestev.equals("+2"))
+            vastanekaart.setTugevus(vastanekaart.getTugevus()+2);
+        if (vastasekestev.equals("-2"))
+            vastanekaart.setTugevus(vastanekaart.getTugevus()-2);
+
+        joonistaKaart(vastanekaart, 200, 200);
+
+        //Muudab meeleshoitud erilised efektid tagasi tühjaks
+        this.mangijakestev = "";
+        this.vastasekestev = "";
+
+        //teeb erilisekaardi muutused ära
+        if (mangitud.getEriline() != null) {
+            Object[] muutused = teeEriline(mangitud, vastanekaart, this.mangija, this.vastane);
+            mangitud = (Kaart) muutused[0];
+            vastanekaart = (Kaart) muutused[1];
+            this.mangija = (Kasi) muutused[2];
+            this.vastane = (Vastane) muutused[3];
+            this.mangijakestev = (String) muutused[4];
+        }
+        if (vastanekaart.getEriline() != null){
+            Object[] muutused = teeEriline(vastanekaart, mangitud, this.vastane, this.mangija);
+            vastanekaart = (Kaart)muutused[0];
+            mangitud = (Kaart)muutused[1];
+            this.vastane = (Vastane) muutused[2];
+            this.mangija = (Kasi)muutused[3];
+            this.vastasekestev = (String)muutused[4];
+        }
+
+        //Prindib kõik mängja kaardid välja
+        joonistaMangijaKaardid();
+
+        //Prindib kõik võidetud kaartide kogused välja
+        joonistaVoiduKogused();
+
+        //Väljastab hetkel kehtivad efektid
+        joonistaEriEfektid();
+
+
+        //Kontrollib kumb kaart võidab
+        int tulemus = mangitud.compareTo(vastanekaart);
+        if (tulemus == 0)
+            System.out.println("Jäite see round viiki.");
+        else if (tulemus > 0) {
+            System.out.println("Selle roundi võitsid");
+            this.mangijadict.put(mangitud.getElement(), this.mangijadict.get(mangitud.getElement())+1);
+        }else {
+            System.out.println("Selle roundi kaotasid");
+            this.vastanedict.put(vastanekaart.getElement(), this.vastanedict.get(vastanekaart.getElement()) + 1);
+        }
+
+
+        //Vastasel ei leidu kaarti mis ei ole blokeeritud
+        if (eiLeiduBlokeerimataElement(this.mangijakestev, this.vastane)){
+            System.out.println("Sinu võit! Vastasel ei ole kaarti, mida saaks käia");
+            tegeleVoit("Mangija");
+            this.mangkaib = false;
+        }
+        //Mängijal ei leidu kaarti mis ei ole blokeeritud
+        if (eiLeiduBlokeerimataElement(this.vastasekestev, this.mangija)){
+            System.out.println("Arvuti võitis! Sul ei ole kaarti, mida saaksid käia");
+            tegeleVoit("Arvuti ");
+            this.mangkaib = false;
+        }
+
+
+        //Kontroll, kas on võidetud kaartidega kombinatisoon
+        if (kasVoidetud(this.mangijadict)) {
+            System.out.println("Sinu võit! :)");
+            tegeleVoit("Mangija");
+            this.mangkaib = false;
+        }
+        if (kasVoidetud(this.vastanedict)) {
+            System.out.println("Arvuti võitis! :(");
+            tegeleVoit("Arvuti ");
+            this.mangkaib = false;
+        }
+        //Prindib kõik mängja kaardid välja
+        joonistaMangijaKaardid();
+
+        //Prindib kõik võidetud kaartide kogused välja
+        joonistaVoiduKogused();
+
+        //Väljastab hetkel kehtivad efektid
+        joonistaEriEfektid();
+    }
+
+    public void tegeleVoit(String tekst){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.INDIGO);
+        gc.fillText(tekst + " Voitis, mang labi", 300, 150);
+
+        //TODO uus aken, mis annab v'imaluse uuesti alustada v]i v;ljuda
+    }
+
+    public void joonistaEriEfektid(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(580, 190, 200, 100);
+        gc.setFill(Color.INDIGO);
+        String[] teksid = efektidValja();
+        gc.fillText("Sinule : " + teksid[0], 600, 200);
+        gc.fillText("Vastasele : " + teksid[1], 600, 230);
+    }
+
+    public void joonistaMangijaKaardid(){
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        int laius = (int)(canvas.getWidth()/10);
+        int korgus = (int)(canvas.getHeight()/6);
+        for (int i = 2; i <= 6; i++) {
+            joonistaKaart(mangija.getKaardid()[i-2],canvas.getWidth()/8 *i - laius/2, canvas.getHeight()*3/4);
+        }
+    }
+
+    public void joonistaVoiduKogused(){
+        String tuleemoji = "\uD83D\uDD25";
+        String veeemoji = "\uD83D\uDCA7";
+        String lumeemoji = "❄";
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.WHITE);
+        gc.fillRect(480, 390, 300, 50);
+        gc.setFill(Color.INDIGO);
+
+
+        String tekst1 = "   " + tuleemoji + " : " + this.mangijadict.get("tuli") + "  "+
+                veeemoji + " : " + this.mangijadict.get("vesi") + "  " + lumeemoji + " : " + this.mangijadict.get("lumi");
+
+        String tekst2 = "   " + tuleemoji + " : " + this.vastanedict.get("tuli") + "  "+
+                veeemoji + " : " + this.vastanedict.get("vesi") + "  " + lumeemoji + " : " + this.vastanedict.get("lumi");
+
+        gc.fillText("Sinule : " + tekst1, 500, 400);
+        gc.fillText("Vastasele : " + tekst2, 500, 430);
     }
 
     public void joonistaKaart(Kaart kaart, double x, double y){
@@ -114,180 +289,6 @@ public class Mang extends Application {
     public static void main(String[] args) throws InterruptedException {
         launch(args);}
 
-        /*
-        int kaik;
-        boolean mangLabi = false;
-        //Kehtivate efektide meeleshoid
-        String mangijakestev = "";
-        String vastasekestev = "";
-
-        //Mängija soovi teadmiseks
-        Scanner scanner = new Scanner(System.in);
-
-        //Võidukaartide kogus
-        HashMap<String, Integer> mangijadict = new HashMap<>();
-        mangijadict.put("tuli", 0);
-        mangijadict.put("vesi", 0);
-        mangijadict.put("lumi", 0);
-        HashMap<String, Integer> vastanedict = new HashMap<>();
-        vastanedict.put("tuli", 0);
-        vastanedict.put("vesi", 0);
-        vastanedict.put("lumi", 0);
-
-
-        //teeme käe, millel on kaardikohad
-        Kasi kaes = new Kasi();
-        kaes.suvalisedKaardidKaes();
-        //Vastasele samamoodi
-        Vastane vastane = new Vastane();
-        vastane.suvalisedKaardidKaes();
-
-
-        System.out.println("Kuidas mängida?");
-        System.out.println("""
-                Mängijale on ette antud 5 kaarti, mille hulgast valitakse üks, mis käiakse välja.\s
-                Vastane (ehk arvuti) käib samal ajal oma kaardi.
-                Ühel kaardil on kindel tugevus (arv 2-st 12-ni) ja element (tuli, vesi või lumi).\s
-                Välja käidud kaartidest võidab see, mis on elemendi või numbri poolest tugevam:\s""");
-        System.out.println("Tuli võidab lume,\nLumi võidab vee,\nVesi võidab tule.");
-        System.out.println("Kui käidud kaartidel on sama element võidab see, kelle kaardil on suurem number. \nKui number ja element on mõlemal samad, " +
-                "siis on tegemist viigiga.");
-        System.out.println("""
-                Peale käiku antakse võitjale tema käidud kaart tagasi, mis läheb võidetud kaartide komplekti.\s
-                Kui käik lõppes viigiga, ei saa kumbki mängija kaarti.
-                Mängija, kes saab esimesena komplektis kokku nii tule, vee kui ka lume elemendiga kaardid, on võitnud.""");
-        System.out.println();
-        System.out.println("""
-                Kaartidel võivad olla ka efektid (üleval paremal), mis mõjutavad võiduvõimalust või järgmist käiku:
-                +2 - järgmisel käigul on mängija käidud kaardi number kahe võrra suurem.
-                -2 - järgmisel käigul on mängija käidud kaardi number kahe võrra väiksem.
-                1\uD83D\uDD019 - väiksema numbriga kaart võidab (kui kaardid on sama elemendiga).
-                \uD83D\uDCA3\uD83D\uDD25 - kõik selle elemendiga kaardid vahetatakse vastase käes välja.
-                \uD83D\uDD25➡\uD83D\uDCA7 - vasakpoolse elemendiga kaardid vahetatakse vastase käes parempoolsete vastu välja.
-                \uD83D\uDEAB❄ - järgmisel käigul ei saa vastane ühtegi selle elemendiga kaarti käia.
-                Kui vastasel on kõik kaardid blokeeritud, kaotab ta mängu automaatselt.""");
-        System.out.println();
-
-        while (!mangLabi) {
-            //Prindib kõik mängja kaardid välja
-            System.out.println("Siin on sinu kaardid: ");
-            kaes.kasiValja();
-
-            //Prindib kõik võidetud kaartide kogused välja
-            System.out.print("Sinu võidukaardid    ");
-            voiduKogus(mangijadict);
-            System.out.print("Vastase võidukaardid ");
-            voiduKogus(vastanedict);
-
-            //Väljastab hetkel kehtivad efektid
-            efektidValja(mangijakestev, vastasekestev);
-
-            System.out.println("Vali, mitmendat kaarti vasakult soovid käia: ");
-            boolean valikkorras = false;
-            kaik = 1;
-            //Kontrollib, et mängija valiks sobiva numbri
-            while (!valikkorras) {
-                kaik = scanner.nextInt();
-                //valitud kaart on sobiva indeksid
-                if (kaik > 0 && kaik < 6) {
-                    //Kaart ei ole blokeeritud
-                    if (kaes.getKaardid()[kaik - 1].getElement().equals(vastasekestev)) {
-                        System.out.println("See element on vastase poolt blokeeritud");
-                    }else {
-                        valikkorras = true;}
-                } else {
-                    System.out.println("Palun vali sobiv number (1-5)");
-                }
-            }
-
-
-            //Prindib mängija käidud kaardi välja
-            System.out.println("Käisid kaardi:");
-            Kaart mangijakaart = kaes.mangiKaart(kaik - 1);
-            //Kui eriline võime oli kaardi tugevust muuta teeb selle siin ära
-            if (mangijakestev.equals("+2"))
-                mangijakaart.setTugevus(mangijakaart.getTugevus()+2);
-            if (mangijakestev.equals("-2"))
-                mangijakaart.setTugevus(mangijakaart.getTugevus()-2);
-            mangijakaart.kaartString();
-            //Et oleks mängu ajal kaarte näha
-            Thread.sleep(1000);
-
-
-            //Prindib vastase käidud kaardi välja
-            System.out.println("Vastane käis kaardi: ");
-            Kaart vastanekaart = vastane.mangiKaart(mangijakestev);
-
-            //Kui eriline võime oli kaardi tugevust muuta teeb selle siin ära
-            if (vastasekestev.equals("+2"))
-                vastanekaart.setTugevus(vastanekaart.getTugevus()+2);
-            if (vastasekestev.equals("-2"))
-                vastanekaart.setTugevus(vastanekaart.getTugevus()-2);
-            vastanekaart.kaartString();
-            //Et oleks mängu ajal kaarte näha
-            Thread.sleep(1000);
-
-            //Muudab meeleshoitud erilised efektid tagasi tühjaks
-            mangijakestev = "";
-            vastasekestev = "";
-
-            //teeb erilisekaardi muutused ära
-            if (mangijakaart.getEriline() != null) {
-                Object[] muutused = teeEriline(mangijakaart, vastanekaart, kaes, vastane);
-                mangijakaart = (Kaart) muutused[0];
-                vastanekaart = (Kaart) muutused[1];
-                kaes = (Kasi) muutused[2];
-                vastane = (Vastane) muutused[3];
-                mangijakestev = (String) muutused[4];
-            }
-            if (vastanekaart.getEriline() != null){
-                Object[] muutused = teeEriline(vastanekaart, mangijakaart, vastane, kaes);
-                vastanekaart = (Kaart)muutused[0];
-                mangijakaart = (Kaart)muutused[1];
-                vastane = (Vastane) muutused[2];
-                kaes = (Kasi)muutused[3];
-                vastasekestev = (String)muutused[4];
-            }
-
-
-            //Kontrollib kumb kaart võidab
-            int tulemus = mangijakaart.compareTo(vastanekaart);
-            if (tulemus == 0)
-                System.out.println("Jäite see round viiki.");
-            else if (tulemus > 0) {
-                System.out.println("Selle roundi võitsid");
-                mangijadict.put(mangijakaart.getElement(), mangijadict.get(mangijakaart.getElement())+1);
-            }else {
-                System.out.println("Selle roundi kaotasid");
-                vastanedict.put(vastanekaart.getElement(), vastanedict.get(vastanekaart.getElement()) + 1);
-            }
-
-
-            //Vastasel ei leidu kaarti mis ei ole blokeeritud
-            if (eiLeiduBlokeerimataElement(mangijakestev, vastane)){
-                System.out.println("Sinu võit! Vastasel ei ole kaarti, mida saaks käia");
-                mangLabi = true;
-            }
-            //Mängijal ei leidu kaarti mis ei ole blokeeritud
-            if (eiLeiduBlokeerimataElement(vastasekestev, kaes)){
-                System.out.println("Arvuti võitis! Sul ei ole kaarti, mida saaksid käia");
-                mangLabi = true;
-            }
-
-
-            //Kontroll, kas on võidetud kaartidega kombinatisoon
-            if (kasVoidetud(mangijadict)) {
-                System.out.println("Sinu võit! :)");
-                mangLabi = true;
-            }
-            if (kasVoidetud(vastanedict)) {
-                System.out.println("Arvuti võitis! :(");
-                mangLabi = true;
-            }
-
-        }
-         */
-
 
     //Kontrollib võitu
     public static boolean kasVoidetud(HashMap asi){
@@ -297,17 +298,6 @@ public class Mang extends Application {
         //kas on igat elementi 1 või siis ühte elementi 3
         return (tuli > 0 && vesi > 0 && lumi > 0) || tuli > 2 || vesi > 2 || lumi > 2;
     }
-
-
-    //Väljastab võidetud kaartide koguse
-    public static void voiduKogus(HashMap dict){
-        String tuleemoji = "\uD83D\uDD25";
-        String veeemoji = "\uD83D\uDCA7";
-        String lumeemoji = "❄";
-        System.out.println("   " + tuleemoji + " : " + dict.get("tuli") + "  "+
-                veeemoji + " : " + dict.get("vesi") + "  " + lumeemoji + " : " + dict.get("lumi"));
-    }
-
 
     //Võtab mängitud kaardidi ja hetkel käesolevad kaardid argumentideks
     //Muudab neid vastavalt erivõimele
@@ -351,7 +341,7 @@ public class Mang extends Application {
 
 
     //Väljastab efektid, mis hetkel toimel on
-    public static void efektidValja(String mangija, String vastane){
+    public String[] efektidValja(){
         String blokemoji = "\uD83D\uDEAB";
         String tuleemoji = "\uD83D\uDD25";
         String veeemoji = "\uD83D\uDCA7";
@@ -362,7 +352,7 @@ public class Mang extends Application {
         String sinule = "";
         String vastasele = "";
 
-        switch (mangija){
+        switch (this.mangijakestev){
             case "+2" -> sinule = sinule + "  +2";
             case "-2" -> sinule = sinule + "  -2";
             case "tuli" -> vastasele = vastasele + "  " + teinekitsas+blokemoji + tuleemoji + kitsas;
@@ -370,7 +360,7 @@ public class Mang extends Application {
             case "lumi" -> vastasele = vastasele + "  " + teinekitsas+kitsas+blokemoji + lumeemoji + kitsas;
             default -> vastasele = vastasele + " ❌  ";
         }
-        switch (vastane){
+        switch (this.vastasekestev){
             case "+2" -> vastasele = vastasele + "  +2";
             case "-2" -> vastasele = vastasele + "  -2";
             case "tuli" -> sinule = sinule + "  " + teinekitsas+blokemoji + tuleemoji + kitsas;
@@ -379,8 +369,7 @@ public class Mang extends Application {
             default -> sinule = sinule + " ❌  ";
         }
 
-        //Prindib välja hetke efektid
-        System.out.println("Hetkel kehtivad efektid - sinule : " + sinule + "  vastasele : " + vastasele);
+        return new String[]{sinule, vastasele};
     }
 
     public static Kasi eemaldaElement(Kaart kaart, Kasi kasi){
